dc7b58e7ff71021a2beaa4822560d519bdbc2ebb
fengnan.xfn <fengnan.xfn@antfin.com>
2019-08-16 20:37:16 +0800

    fix: bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var plugin = require("./plugin");
 module.exports = function(PluginHost) {
   var app = PluginHost.owner;
 
-  if (!app.options.declaration.em) {
+  if (!app.options.declarations.em) {
     app.options.addDeclaration({ name: 'external-modulemap', short: 'em' });
     app.converter.addComponent('external-module-map', plugin.ExternalModuleMapPlugin);
   }
